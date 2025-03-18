586357fba93d3016cf264506c342afcb67218f10
dosunamingo <sunashun5@gmail.com>
2025-03-18 23:26:10 +0900

    a

---
+++ Assets/c#/buttondata.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttondata
+    : MonoBehaviour
+{
+    public Button targetButton;
+
+    [SerializeField] GameObject banana;
+
+    //‰Ÿ‚µ‚½‚Æ‚«‚ÉŒÄ‚Î‚ê‚éŠÖ”
+    public void OnClick()
+    {
+        
+       
+             
+        //banana‚Ì¢Š«
+        BananaSpawn();
+
+    }
+void BananaSpawn() {
+        Vector3 buttonworldPos=targetButton. transform.position;
+        _ = Instantiate(banana,new Vector3(buttonworldPos.x, 540, -1
+             ), transform.rotation);
+            }
+
+}
