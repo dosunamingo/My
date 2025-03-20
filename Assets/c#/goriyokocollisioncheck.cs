625d759d167c232083fc4b317cffdd14995e270c
dosunamingo <sunashun5@gmail.com>
2025-03-20 10:52:15 +0900

    a

--- Assets/c#/goriyokocollisioncheck.cs
+++ Assets/c#/goriyokocollisioncheck.cs
@@ -9,6 +9,12 @@ public class goriyokocollisioncheck : MonoBehaviour
     // Start is called before the first frame update
     private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
     {
-        if (collision.gameObject.name == "migikabe"||collision.gameObject.name=="hidarikabe") { ison = !ison; }
+        if (collision.gameObject.name == "migikabe"||collision.gameObject.name=="hidarikabe") { ison = true; }
+   
+    }
+    private void OnCollisionExit2D(UnityEngine.Collision2D collision)
+    {
+        if (collision.gameObject.name == "migikabe" || collision.gameObject.name == "hidarikabe") { ison = false; }
+
     }
 }
